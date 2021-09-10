cade46bea1d63f8ff64bfb123781f30ffad27a81
Cecilia Martinez <cecimart3@gmail.com>
2021-09-09 23:25:14 -0400

    backend: GET list of all warehouses

--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const app = express();
-const videoRoute = require("./routes/videos");
 const cors = require("cors");
 
 require("dotenv").config();
@@ -8,7 +7,9 @@ require("dotenv").config();
 app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
-app.use("/", videoRoute);
+
+const warehousesRoute = require('./routes/warehouse/warehouse')
+app.use("/warehouses", warehousesRoute);
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
--- server/routes/warehouse/warehouse.js
+++ server/routes/warehouse/warehouse.js
@@ -2,3 +2,20 @@ const express = require("express");
 const router = express.Router();
 const fs = require("fs");
 const { v4: uuidv4 } = require("uuid");
+
+const getWarehouses = () => {
+    const warehouses = fs.readFileSync('./data/warehouses.json')
+    return JSON.parse(warehouses)
+}
+
+
+router.get('/', (req, res) => {
+    try {
+        const warehouses = getWarehouses()
+        return res.status(200).json(warehouses)
+    } catch (error) {
+        return res.status(500).send({ error: 'Information cannot be found.'})
+    }
+})
+
+module.exports = router
