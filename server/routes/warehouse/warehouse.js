9f4de96565d8974f7e3c1380f779d6c800153849
Kory Koponen <koponenk@gmail.com>
2021-09-10 20:53:32 -0500

    back-end post functionality

--- server/routes/warehouse/warehouse.js
+++ server/routes/warehouse/warehouse.js
@@ -8,7 +8,38 @@ const getWarehouses = () => {
     return JSON.parse(warehouses)
 }
 
+const warehouseDataPath = "./data/warehouses.json";
+const readWarehouseData = fs.readFileSync(warehouseDataPath);
+const parsedWarehouseData = JSON.parse(readWarehouseData);
 
+const readWarehouseFile = () => {
+  return parsedWarehouseData;
+};
+
+router.post("/add", (req, res) => {
+  if (
+    !req.body.warehouse_name ||
+    !req.body.street ||
+    req.body.city ||
+    req.body.country ||
+    req.body.contact
+  )
+    res.status(200).send("request info not complete");
+  const addWarehouse = {
+    id: uuidv4(),
+    name: req.body.warehouse_name,
+    address: req.body.street,
+    city: req.body.city,
+    country: req.body.country,
+    contact: req.body.contact,
+  };
+
+  const newWarehouseData = readWarehouseFile();
+  newWarehouseData.push(addWarehouse);
+  fs.writeFileSync(warehouseDataPath, JSON.stringify(newWarehouseData));
+
+  return res.status(201).json(newWarehouseData);
+});
 router.get('/', (req, res) => {
     try {
         const warehouses = getWarehouses()
