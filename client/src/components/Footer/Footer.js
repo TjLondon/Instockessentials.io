4a314073379199f7b88ec2ff8b886dc8ea277c94
softwareiscool <72666846+softwareiscool@users.noreply.github.com>
2021-09-11 15:12:54 -0700

    html done

---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+//The footer component
+import './Footer.scss';
+
+const Footer = () => {
+    return (  
+        <div className='footer'>
+            <h6 className='footer__text'>&copy; InStock Inc. All Rights Reserved.</h6>
+        </div>
+    );
+}
+ 
+export default Footer;
