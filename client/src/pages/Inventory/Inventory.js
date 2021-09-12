3a4d86e65a5514a15f4262778aa3ac07b3e7cee6
Andycsong <andysong96@hotmail.com>
2021-09-11 20:34:44 -0400

    TRYING TO FIX THE DEVELOP APPROVED BY ALL

--- client/src/pages/Inventory/Inventory.js
+++ client/src/pages/Inventory/Inventory.js
@@ -1,7 +1,6 @@
-<<<<<<< HEAD
 import React, { Component } from 'react'
 import AddInventory from '../AddInventory/AddInventory'
-import instockRequests from '../../utilities/apiCalls' 
+import instockRequests from '../../utilities/apiCalls'
 
 class Inventory extends Component {
     state = {
@@ -9,52 +8,39 @@ class Inventory extends Component {
         selectedInventory: null,
     }
     // getInventory(warehouseID) { 
-    getInventory(id) { 
+    getInventory(id) {
         instockRequests.getAllInventories(id)
-        .then((response) => {
-            console.log('RESPONSE DATA:::', response.data)
-            console.log('RESPONSE ONLY :::', response)
-            this.setState({
-                inventories: response.data
+            .then((response) => {
+                console.log('RESPONSE DATA:::', response.data)
+                console.log('RESPONSE ONLY :::', response)
+                this.setState({
+                    inventories: response.data
+                })
+                console.log('INVENTORIES RESPONSE DATA::: ', this.state.inventories)
             })
-            console.log('INVENTORIES RESPONSE DATA::: ', this.state.inventories)
-        }) 
     }
     componentDidMount() {
         instockRequests.getAllWarehouses()
-        .then((response) => {
-            this.setState({
-                inventories: response.data,
+            .then((response) => {
+                this.setState({
+                    inventories: response.data,
+                })
+                const id = this.props.match.params.id || response.data[0].id;
+                console.log('PARAMS ID: WAREHOUSE ID:: ',)
+                this.getInventory(id)
             })
-            const id = this.props.match.params.id || response.data[0].id;
-            console.log('PARAMS ID: WAREHOUSE ID:: ', )
-            this.getInventory(id)
-        })
     }
-    componentDidUpdate(warehouseID) { 
-        if(warehouseID.match.params.id !== this.props.match.params.id) {
+    componentDidUpdate(warehouseID) {
+        if (warehouseID.match.params.id !== this.props.match.params.id) {
             this.getInventory(this.props.match.params.id)
         }
     }
     render() {
         return (
             <div className="inventories">
-                <AddInventory inventories={this.state.inventories}/>
+                <AddInventory inventories={this.state.inventories} />
             </div>
         )
     }
 }
 export default Inventory
-=======
-import React from 'react';
-
-const Inventory = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-};
-
-export default Inventory;
->>>>>>> cf4b6dee5a4192687d1947c7634dd9f15f5e4d1f
