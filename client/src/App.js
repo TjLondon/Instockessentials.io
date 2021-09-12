df133326fbc1e8479f97527b0f3100c9c170b380
Andycsong <andysong96@hotmail.com>
2021-09-12 19:58:32 -0400

    Modal Component visual design finished. Functionality not connected yet

--- client/src/App.js
+++ client/src/App.js
@@ -17,6 +17,7 @@ class App extends Component {
 
     return (
       <BrowserRouter>
+        <Header />
         <Switch>
           <Route path='/' exact component={Warehouse} />
           <Route path='/:id' exact component={DetailsWarehouse} />
--- client/src/pages/Warehouse/Warehouse.js
+++ client/src/pages/Warehouse/Warehouse.js
@@ -1,9 +1,9 @@
 import React from 'react';
+import DeleteModal from '../../components/DeleteModal/DeleteModal'
 
 const Warehouse = () => {
     return (
         <div>
-            
         </div>
     );
 };
