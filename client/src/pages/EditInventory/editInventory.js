0b8e826b462f6f816cf62ef15a577d41b1734701
softwareiscool <72666846+softwareiscool@users.noreply.github.com>
2021-09-12 02:12:22 -0700

    styling finished

--- client/src/pages/EditInventory/editInventory.js
+++ client/src/pages/EditInventory/editInventory.js
@@ -7,57 +7,57 @@ function EditInventory() {
 
             <section className='head'>
                 <img className='head__arrow' src='http://localhost:8080/Assets/Icons/arrow_back-24px.svg' alt='arrow back'/>
-                <h2 className='head__title'>Add New Inventory</h2>
+                <h2 className='head__title'>Edit Inventory Item</h2>
             </section>
 
             <section className='option'>
 
                 <section className='details'>
+                    <h2 className='details__title'>Item Details</h2>
                     <div className='details__item'>
-                        <label className='details__item-name'>Item Name</label>
-                        <input className='details__item-input'/>
+                        <label className='details__item-name'>Item Name</label><br/>
+                        <input className='details__item-input' placeholder='Television'/>
                     </div>
                     <div className='details__description'>
-                        <label className='details__description-name'>Description</label>
-                        <input className='details__description-input'/>
+                        <label className='details__description-name'>Description</label><br/>
+                        <input className='details__description-input' placeholder=''/>
                     </div>
-                    <div className='details__catagory'>
-                        <label className='details__catagory-name'>Catagory</label>
-                        <input classname='details__catagory-select'>
-                            <img className='details__catagory-pic' src='http://localhost:8080/Assets/Icons/arrow_drop_down-24px.svg'/>
-                        </input>
+                    <div className='details__category'>
+                        <label className='details__category-name'>Category</label><br/>
+                        <input className='details__category-input' placeholder='Please Select'/>
                     </div>
                 </section>
 
                 <section className='ava'>
-                    <h6 className='ava__title'>Status</h6>
-                    <section className='ava__option'>
-                        <div className='ava__instock'>
-                            <input className='ava__instock-option' type='radio'/>
-                            <label className='ava__instock-text' for='ava_instock-option'>In stock</label>
-                        </div>
+                    
+                    <h2 className='ava__title'>Item Availability</h2>
 
-                        <div className='ava__outstock'>
-                            <input className='ava__outstock-option' type='radio'/>
-                            <label className='ava__outstock-text' for='ava_instock-option'>Out of stock</label>
+                    <section className='ava__option'>
+                        <h6 className='ava__status'>Status</h6>
+                        <div className='ava__stockflex'>
+                            <div className='ava__instock'>
+                                <input className='ava__instock-option' type='radio'/>
+                                <label className='ava__instock-text' for='ava_instock-option'>In stock</label>
+                            </div>
+                            <div className='ava__outstock'>
+                                <input className='ava__outstock-option' type='radio'/>
+                                <label className='ava__outstock-text' for='ava_instock-option'>Out of stock</label>
+                            </div>
                         </div>
                     </section>
                     
                     <section className='ava__house'>
-                        <label className='ava__house-title'>Warehouse</label>
-                        <input classname='ava__house-dropdown'>
-                            <img className='ava_house-pic' src='http://localhost:8080/Assets/Icons/arrow_drop_down-24px.svg'/>
-                        </input>
+                        <label className='ava__house-title'>Warehouse</label><br/>
+                        <input className='ava__house-dropdown' placeholder='Manhattan'/>
                     </section>
-                </section>
-
-                <section className='button'>
-                    <button className='button__cancel'>Cancel</button>
-                    <button className='button__save'>+ Add Item</button>
                 </section>
 
             </section>
             
+            <section className='button'>
+                <button className='button__cancel'>Cancel</button>
+                <button className='button__add'>Save</button>
+            </section>       
         </div>
     )
 }
