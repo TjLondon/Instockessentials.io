3a79d16ed457dc60df0e98b448d3c72b24770ee7
Andycsong <andysong96@hotmail.com>
2021-09-12 19:59:23 -0400

    Put route created for warehouse details

--- server/routes/warehouse/warehouse.js
+++ server/routes/warehouse/warehouse.js
@@ -4,21 +4,22 @@ const fs = require("fs");
 const { v4: uuidv4 } = require("uuid");
 
 const warehouseFilePath = "./data/warehouses.json"
+const putWarehouseFilePath = require("../../data/warehouses.json")
 
-const readWarehouseData = () => {
-  const warehouseData = fs.readFileSync(warehouseFilePath);
-  const parsedWarehouseFile = JSON.parse(warehouseData)
-  return parsedWarehouseFile
-}
+// const readWarehouseData = () => {
+//   const warehouseData = fs.readFileSync(warehouseFilePath);
+//   const parsedWarehouseFile = JSON.parse(warehouseData)
+//   return parsedWarehouseFile
+// }
 
 const getWarehouses = () => {
   const warehouses = fs.readFileSync('./data/warehouses.json')
   return JSON.parse(warehouses)
 }
 
-const readWarehouseFile = () => {
-  return parsedWarehouseData;
-};
+// const readWarehouseFile = () => {
+//   return parsedWarehouseData;
+// };
 
 router.post("/add", (req, res) => {
   if (
@@ -35,15 +36,54 @@ router.post("/add", (req, res) => {
     address: req.body.street,
     city: req.body.city,
     country: req.body.country,
-    contact: req.body.contact,
+    contact: {
+      name: req.body.contact.name,
+      position: req.body.contact.position,
+      phone: req.body.contact.phone,
+      email: req.body.contact.email
+    }
   };
 
-  const newWarehouseData = readWarehouseFile();
+  const newWarehouseData = getWarehouses();
   newWarehouseData.push(addWarehouse);
-  fs.writeFileSync(warehouseDataPath, JSON.stringify(newWarehouseData));
+  fs.writeFileSync(warehouseFilePath, JSON.stringify(newWarehouseData));
 
   return res.status(201).json(newWarehouseData);
 });
+
+
+// j21dm-14 Put/Patch/Edit a Warehouse--This is used for warehouse details
+router.put('/:id', ((req, res) => {
+  let { id } = req.params;
+  const editWarehouse = {
+    "id": id,
+    "name": req.body.name,
+    "address": req.body.address,
+    "city": req.body.city,
+    "country": req.body.country,
+    "contact": {
+      "name": req.body.contact.name,
+      "position": req.body.contact.position,
+      "phone": req.body.contact.phone,
+      "email": req.body.contact.email
+    }
+  }
+  const editWarehouseData = putWarehouseFilePath.map(warehouse => {
+    if (warehouse.id === id) {
+      return warehouse = editWarehouse
+    } else {
+      return warehouse = warehouse
+    }
+  })
+  fs.writeFile(`${warehouseFilePath}`, JSON.stringify(editWarehouseData), (err) => {
+    if (err) {
+      console.log(err)
+    } else {
+      res.status(200).json('Warehouse has been updated')
+    }
+  })
+}))
+
 router.get('/', (req, res) => {
   try {
     const warehouses = getWarehouses()
@@ -55,7 +95,7 @@ router.get('/', (req, res) => {
 router.delete('/:id', (req, res) => {
 
   const deleteId = req.params.id
-  const parsedWarehouse = readWarehouseData();
+  const parsedWarehouse = getWarehouses();
   const findWarehouse = parsedWarehouse.findIndex((warehouse) => warehouse.id === deleteId);
   if (!findWarehouse) {
     return res.status(404).send('No warehouse associated with that id');
@@ -65,4 +105,6 @@ router.delete('/:id', (req, res) => {
   res.status(200).send("Delete Successful");
 
 })
+
+
 module.exports = router
