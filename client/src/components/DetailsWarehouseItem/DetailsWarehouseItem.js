924084e5cbf1716898c62041c5776baa75f4e710
softwareiscool <72666846+softwareiscool@users.noreply.github.com>
2021-09-12 17:06:42 -0700

    regular

---
+++ client/src/components/DetailsWarehouseItem/DetailsWarehouseItem.js
@@ -0,0 +1,51 @@
+import './DetailsWarehouseItem.scss'
+import { Link } from 'react-router-dom'
+
+const DetailsWarehouseItem = (warehouses) => {
+    return (
+        <li key={warehouses.id} className='item'>
+            <div className='item__container'>
+                <div className='item__column'>
+                    <div className='item__category-container'>
+                        <h5 className='item__category'>WAREHOUSES</h5>
+                        <div className='item__link item__content'>
+                            <Link to={`/${warehouses.id}`} >{warehouses.name}</Link>
+                            <img src='http://localhost:8080/Assets/Icons/chevron_right-24px.svg' alt='arrow' />
+                        </div>
+                    </div>
+                    <div className='item__category-container'>
+                        <h5 className='item__category'>ADDRESS</h5>
+                        <div className='item__content'>
+                            <p className='item__info'>{warehouses.address},</p>
+                            <p className='item__info'>{warehouses.city}, {warehouses.country}</p>
+                        </div>
+                    </div>
+                </div>
+                <div className='item__column'>
+                    <div className='item__category-container'>
+                        <h5 className='item__category'>CONTACT NAME</h5>
+                        <div className='item__content'>
+                            <p className='item__info'>{warehouses.contact.name}</p>
+                        </div>
+                    </div>
+                    <div className='item__category-container'>
+                        <h5 className='item__category'>CONTACT INFORMATION</h5>
+                        <div  className='item__content'>
+                            <p className='item__info'>{warehouses.contact.phone}</p>
+                            <p className='item__info'>{warehouses.contact.email}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className='item__actions'>
+                <h5 className='item__category item__category--actions'>ACTIONS</h5>
+                <div className='item__icons'>
+                    <img className='item__icon' src='http://localhost:8080/Assets/Icons/delete_outline-24px.svg' alt='delete button'/>
+                    <img className='item__icon' src='http://localhost:8080/Assets/Icons/edit-24px.svg' alt='edit button'/>
+                </div>
+            </div>
+        </li>
+    )
+}
+
+export default DetailsWarehouseItem;
--- client/src/pages/DetailsWarehouse/detailsWarehouse.js
+++ client/src/pages/DetailsWarehouse/detailsWarehouse.js
@@ -1,96 +1,37 @@
-//Building up the warehouse details compoenent.
-import './DetailsWarehouse.scss';
+import './DetailsWarehouse.scss'
+import { Component } from 'react'
+import { Link } from 'react-router-dom'
+import SearchBar from '../../components/SearchBar/SearchBar'
+import DetailsWarehousesList from '../../components/DetailsWarehousesList/DetailsWarehousesList'
+import instockRequests from '../../utilities/apiCalls'
 
+class Warehouse extends Component {
+    state = {
+        warehouses: []
+    }
 
-const DetailsWarehouse = () => {
+    componentDidMount() {
+        instockRequests.getAllWarehouses().then(response => {
+            this.setState({
+                warehouses: response.data
+            })
+        }).catch(error => console.log(error))
+    }
 
-    return ( 
-        <section className='details'>
-
-            <section className='head'>
-                <div className='head__main'>
-                    <img className='head__arrow' src='http://localhost:8080/Assets/Icons/arrow_back-24px.svg' alt = 'arrow to go back'/>
-                    <h2 className='head__title'>King West</h2>
+    render() {
+        return (
+            <div className='warehouses'>
+                <div className='warehouses__header'>
+                    <h1 className='warehouses__title'>Warehouses</h1>
+                    <div className='warehouses__container'>
+                        <SearchBar className='warehouses__search' />
+                        <Link to='/add' className='warehouses__button'>+ Add New Warehouse</Link>
+                    </div>                
                 </div>
-                <button className='head__action'>
-                    <img className='head__edit' src='http://localhost:8080/Assets/Icons/edit-white-24px.svg' alt = 'editing symbol' />
-                    <p className='head__edit-text'>Edit</p>
-                </button>
-            </section>
-
-            <section className='info'>
-                <div className='info__address'>
-                    <h6 className='info__address-title'>WAREHOUSE ADDRESS:</h6>
-                    <p className='info__address-text'>469 King Street West, <br/>Toronto, CAN</p>
-                </div>
-                <div className='info__contact'>
-                    <div className='info__name'>
-                        <h6 className='info__name-title'>CONTACT NAME:</h6>
-                        <p className='info__name-text'>Graeme Lyon <br/>Warehouse Manager</p>
-                    </div>
-                    <div className='info__information'>
-                        <h6 className='info__information-title'>CONTACT INFORMATION</h6>
-                        <p className='info__information-text'>+1 (647) 504-0911<br/>glyon@instock.com</p>
-                    </div>
-                </div>
-            </section>
-
-            <section className='item'>
-
-                <section className='option'>
-
-                    <div className='option__class'>
-                        <div className='option__inventory'>
-                            <div className='option__inventory-status'>
-                                <h6 className='option__inventory-title'>INVENTORY ITEM</h6>
-                                <img className='option__inventory-pic' src='http://localhost:8080/Assets/Icons/sort-24px.svg'/>
-                            </div>
-                            <div className='option__inventory-text'>
-                                <p className='option__inventory-text--blue'>Television</p>
-                                <img className='option__inventory-image' src='http://localhost:8080/Assets/Icons/chevron_right-24px.svg' alt='arrow to the right' />
-                            </div>
-                        </div>
-                        <div className='option__category'>
-                            <div className='option__category-status'>
-                                <h6 className='option__category-title'>CATEGORY</h6>
-                                <img className='option__category-pic' src='http://localhost:8080/Assets/Icons/sort-24px.svg'/>
-                            </div>
-                            <p className='option__category-text'>Electronics</p>
-                        </div>
-                    </div>
-
-                    <div className='option__property'>
-                        <div className='option__status'>
-                            <div className='option__stats'>
-                                <h6 className='option__stats-title'>STATUS</h6>
-                                <img className='option__stats-pic' src='http://localhost:8080/Assets/Icons/sort-24px.svg'/>
-                            </div>
-                            <p className='option__status-text'>IN STOCK</p>
-                        </div>
-
-                        <div className='option__status-qty'>
-                            <div className='option__qty'>
-                                <h6 className='option__qty-title'>QTY</h6>
-                                <img className='option__qty-pic' src='http://localhost:8080/Assets/Icons/sort-24px.svg'/>
-                            </div>
-                            <p className='option__qty-text'>500</p>
-                        </div>
-                    </div>
-
-                </section>
-
-                <section className='icons'>
-                    <h6 className='icons__title'>ACTIONS</h6>
-                    <div className='icons__pic'>
-                        <img className='icons__trash'src='http://localhost:8080/Assets/Icons/delete_outline-24px.svg' alt ='trash can'/>
-                        <img className='icons__pencil' src='http://localhost:8080/Assets/Icons/edit-24px.svg' alt='pencil symbol'/>
-                    </div>
-                </section>
-
-            </section>
-
-        </section>
-     );
+                <DetailsWarehousesList warehouses={this.state.warehouses} />
+            </div>
+        )
+    }
 }
- 
-export default DetailsWarehouse;
+
+export default Warehouse;
